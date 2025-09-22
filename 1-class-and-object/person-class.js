0de8f3b2ce3cb48ebfb95d2212b7b16cab13b95c
Fortuotu <ott.saluvere@gmail.com>
2025-09-22 14:38:11 +0300

    BMI for the Person class

--- 1-class-and-object/person-class.js
+++ 1-class-and-object/person-class.js
@@ -1,17 +1,24 @@
-
 class Person {
-    #name = null
+    #name = null;
     #age = 0;
+    #weight = 0;
+    #height = 0;
 
-    constructor(name, age) {
+    constructor(name, age, weight, height) {
         this.#name = name;
         this.#age = age;
+        this.setWeight(weight);
+        this.setHeight(height);
+    }
+
+    print() {
+        console.log(`Name: ${this.#name}, Age: ${this.#age}`);
     }
 
     setName(name) {
         this.#name = name;
     }
-    
+
     getName() {
         return this.#name;
     }
@@ -22,6 +29,40 @@ class Person {
 
     getAge() {
         return this.#age;
+    }
+
+    setWeight(weight) {
+        if (weight <= 0) {
+            console.log("Kaal peab olema positiivne arv!");
+        } else {
+            this.#weight = weight;
+        }
+    }
+
+    getWeight() {
+        return this.#weight;
+    }
+
+    setHeight(height) {
+        if (height <= 0) {
+            console.log("Pikkus peab olema positiivne arv!");
+        } else {
+            this.#height = height;
+        }
+    }
+
+    getHeight() {
+        return this.#height;
+    }
+
+    getBMI() {
+        if (this.#weight > 0 && this.#height > 0) {
+            let bmi = this.#weight / (this.#height * this.#height);
+            return Number(bmi.toFixed(2));
+        } else {
+            console.log("Kehamassiindeksi arvutamiseks peab olema määratud korrektne kaal ja pikkus.");
+            return null;
+        }
     }
 }
 
--- 1-class-and-object/person-object.js
+++ 1-class-and-object/person-object.js
@@ -1,4 +1,8 @@
 const { Person } = require("./person-class")
 
-const p = new Person("idk", 69);
-console.log(`Name: ${p.getName()}\nAge: ${p.getAge()}`);
+let person = new Person("John Doe", 30, 75, 1.8);
+person.print();
+console.log("BMI:", person.getBMI());
+
+person.setWeight(-5);  // Kuvab veateate "Kaal peab olema positiivne arv!"
+person.setHeight(0);   // Kuvab veateate "Pikkus peab olema positiivne arv!"
