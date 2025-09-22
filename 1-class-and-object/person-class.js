ba11308e6b3e6a6fd6c53452d60571b2eb5cfe53
Fortuotu <ott.saluvere@gmail.com>
2025-09-22 12:10:59 +0300

    create basic project

---
+++ 1-class-and-object/person-class.js
@@ -0,0 +1,28 @@
+
+class Person {
+    #name = null
+    #age = 0;
+
+    constructor(name, age) {
+        this.#name = name;
+        this.#age = age;
+    }
+
+    setName(name) {
+        this.#name = name;
+    }
+    
+    getName() {
+        return this.#name;
+    }
+
+    setAge(age) {
+        this.#age = age;
+    }
+
+    getAge() {
+        return this.#age;
+    }
+}
+
+module.exports = { Person };
---
+++ 1-class-and-object/person-object.js
@@ -0,0 +1,4 @@
+const { Person } = require("./person-class")
+
+const p = new Person("idk", 69);
+console.log(`Name: ${p.getName()}\nAge: ${p.getAge()}`);
